8804eb77f6bf6a9ad401f202d6bffbf4cb7dcb98
Morgan Walder <71555090+Enmn@users.noreply.github.com>
2022-12-14 06:56:13 +0300

    Added new file badge.js
    Added new file badge.js

---
+++ commands/badge.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("badge")
+        .setDescription("Send anything"),
+    async execute(interaction) {
+        await interaction.reply(`🎉 Congratulations, you have completed the steps, now all you have to do is log in to this site to claim your badge\n<https://discord.com/developers/active-developer>\n\nIf it does not work for you, all you have to do is wait **24 hours**`)
+    }
+}
